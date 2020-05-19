466544542ee2003bb40a8ce6ffaf0979eb96b34c
galancoder <usuario@usuario-PC.home>
2020-05-19 22:22:29 +0200

    commit

---
+++ Ejercicios 2º evaluación/src/Unidad_6/Ejercicio_1.java
@@ -0,0 +1,10 @@
+package Unidad_6;
+
+public class Ejercicio_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Ejercicios 2º evaluación/src/unidad6/Excepciones/Ejercicio_3.java
@@ -0,0 +1,38 @@
+package unidad6.Excepciones;
+
+import java.util.Scanner;
+
+public class Ejercicio_3 {
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner entrada= new Scanner(System.in);
+		
+		
+		double A;
+		
+		double B;
+		
+		double C;
+		
+		String linea;
+		
+		while(!((linea=entrada.nextLine().trim()).equalsIgnoreCase("f"))) {
+			
+			if(linea.equalsIgnoreCase("a")) {
+				
+				String [] comando= linea.split(" ");
+				
+				
+			}
+			
+			
+			
+		}
+		
+	}
+
+}
